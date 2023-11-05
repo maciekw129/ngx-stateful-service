6fa4d87ea9dc916283c8d2c61daed5396e220a7e
Maciej Walecki <maciejwalecki99@gmail.com>
2023-11-05 09:28:13 +0100

    refactor: improved module typing

--- projects/ngx-stateful-service/src/lib/ngx-stateful-service.module.ts
+++ projects/ngx-stateful-service/src/lib/ngx-stateful-service.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import {StatefulServiceConfig} from "./ngx-stateful-service.model";
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {InitialState, StatefulServiceConfig} from "./ngx-stateful-service.model";
 import {INITIAL_STATE} from "./ngx-stateful-service.tokens";
 import {NgxStatefulService} from "./ngx-stateful.service";
 
 @NgModule()
 export class NgxStatefulServiceModule {
-  static withConfig(config: StatefulServiceConfig) {
+  static withConfig(config: StatefulServiceConfig): ModuleWithProviders<NgxStatefulServiceModule> {
     return {
       ngModule: NgxStatefulServiceModule,
       providers: [
-        NgxStatefulService<typeof config.initialState>,
+        NgxStatefulService,
         {
           provide: INITIAL_STATE,
           useValue: config.initialState
