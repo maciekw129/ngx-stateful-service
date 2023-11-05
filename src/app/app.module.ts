bee4c85b8428f90b7e50e7de3a935760771cbe31
Maciej Walecki <maciejwalecki99@gmail.com>
2023-11-05 12:03:21 +0100

    feat: added stateful service usage demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
-import { NgModule } from '@angular/core';
+import {importProvidersFrom, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {DemoComponent} from "./demo/demo.component";
+import {RouterModule, RouterOutlet} from "@angular/router";
+import {NgxStatefulServiceModule} from "ngx-stateful-service";
+import {DemoState} from "./demo/demo.model";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DemoComponent,
+    RouterOutlet,
+    RouterModule.forRoot([
+      {
+        path: '',
+        loadComponent: () => import('./demo/demo.component').then(m => m.DemoComponent),
+        providers: [
+          importProvidersFrom(NgxStatefulServiceModule.withConfig<DemoState>({
+            initialState: {
+              todos: []
+            }
+          }))
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo/demo.component.ts
@@ -0,0 +1,44 @@
+import {Component, inject} from '@angular/core';
+import {NgxStatefulService} from "ngx-stateful-service";
+import {DemoState} from "./demo.model";
+import {CommonModule} from "@angular/common";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-demo',
+  templateUrl: './demo.component.html',
+  styleUrls: ['./demo.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ]
+})
+export class DemoComponent {
+  private readonly demoStatefulService = inject(NgxStatefulService<DemoState>);
+  private readonly fb = inject(FormBuilder);
+
+  public form = this.fb.group({todo: ['', Validators.required]});
+  public readonly todos$ = this.demoStatefulService.getStateSlice$('todos');
+
+  public addTodo(todo: string): void {
+    this.demoStatefulService.patchState({
+      todos: [...this.demoStatefulService.getStateSliceValue('todos'), todo]
+    })
+  }
+
+  public removeTodo(todoIndex: number): void {
+    this.demoStatefulService.patchState({
+      todos: this.demoStatefulService.getStateSliceValue("todos").filter((_, index) => index !== todoIndex)
+    })
+  }
+
+  public submitForm() {
+    const todoValue = this.form.controls.todo.value;
+
+    if(todoValue !== null) {
+      this.addTodo(todoValue);
+      this.form.reset();
+    }
+  }
+}
