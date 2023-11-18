1da0854b4a504fb2c1b67c96be441014430b58fd
Maciej Walecki <maciejwalecki99@gmail.com>
2023-11-18 20:29:07 +0100

    feat: added reset state methods

--- projects/ngx-stateful-service/src/lib/ngx-custom-stateful-service.ts
+++ projects/ngx-stateful-service/src/lib/ngx-custom-stateful-service.ts
@@ -6,15 +6,17 @@ import { INITIAL_STATE } from './ngx-stateful-service.tokens';
 @Injectable()
 export abstract class CustomStatefulService<T extends InitialState> {
   protected declare _state$: BehaviorSubject<T>;
+  protected declare _initialState: T;
 
   constructor(
     initialState: T = <T>inject(INITIAL_STATE, { optional: true } ?? null)
   ) {
-    if (Boolean(initialState)) {
-      this._state$ = new BehaviorSubject(initialState);
-    } else {
+    if (!Boolean(initialState)) {
       this.throwEmptyInitialStateError();
     }
+
+    this._initialState = initialState;
+    this._state$ = new BehaviorSubject(initialState);
   }
 
   public getWholeStateValue(): T {
@@ -25,12 +27,23 @@ export abstract class CustomStatefulService<T extends InitialState> {
     return this._state$.asObservable();
   }
 
+  public resetWholeState(): void {
+    this._state$.next(this._initialState);
+  }
+
   public getStateSliceValue<K extends keyof T>(key: K): T[K] {
     return this._state$.value[key];
   }
 
   public getStateSlice$<K extends keyof T>(key: K): Observable<T[K]> {
     return this.getWholeState$().pipe(map((state: T) => state[key]));
+  }
+
+  public resetStateSlice<K extends keyof T>(key: K): void {
+    this._state$.next({
+      ...this._state$.value,
+      [key]: this._initialState[key],
+    });
   }
 
   public patchState(stateSlice: Partial<T>) {
